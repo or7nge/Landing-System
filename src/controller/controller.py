f9b22ab7b77d823a8d26dc5ba4309662836c43be
orange <adgrom@mail.ru>
2024-10-07 10:05:07 +0300

    Temporarily disabled controller.py

--- src/controller/controller.py
+++ src/controller/controller.py
@@ -2,6 +2,8 @@ from drone_commands import execute_directive, connect_to_drone
 
 
 def start_controller_loop(queue):
+    return  # Currently disabled to text the detection system
+
     vehicle = connect_to_drone()  # Connect to the drone when starting the controller loop
     current_directive = None
     while True:
